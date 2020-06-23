3523c0e449a1b4ef68fcac7a981e2dd97086941a
Joao Campos <j.campos4g@gmail.com>
2020-06-23 15:07:23 -0400

    initial commit

---
+++ src/components/IndexPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function IndexPage() {
+
+    return <h1>Welcom to Wall App</h1>
+}
+
+export default IndexPage
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import IndexPage from './components/IndexPage'
+import SignUpPage from './components/SignUpPage' 
+import LoginPage from './components/LoginPage'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+    return (
+        <main>
+            <Navbar />
+            <Switch>
+                <Route path='/' component={IndexPage} exact />
+                <Route path='/register' component={SignUpPage} />
+                <Route path='/login' component={LoginPage} />
+                <Route component={Error} />
+            </Switch>
+        </main>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<BrowserRouter>
+                    <App />
+                </BrowserRouter>, 
+                document.getElementById('root'))
